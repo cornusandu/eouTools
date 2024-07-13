d735d29772dd21144862f65d319d4e0c8c7c419d
cornusandu <34373732+cornusandu@users.noreply.github.com>
2024-07-13 17:29:16 +0300

    new `@memoize` decorator

--- eouTools/decorators.py
+++ eouTools/decorators.py
@@ -1,5 +1,26 @@
-from typing import Callable, Any
+from typing import Callable, Any, Dict
 from functools import wraps
+
+class MemoizedFunction:
+    def __init__(self, func: Callable):
+        self.func = func
+        self.cache: Dict[str, Any] = {}
+
+    def delete_cache(self) -> None:
+        self.cache = {}
+
+    def __call__(self, *args, **kwargs) -> Any:
+        key = str(args) + str(kwargs)
+        if key not in self.cache:
+            self.cache[key] = self.func(*args, **kwargs)
+        return self.cache[key]
+
+    def __getattr__(self, name: str) -> Any:
+        return getattr(self.func, name)
+
+def memoize(func: Callable) -> MemoizedFunction:
+    memoized_function = MemoizedFunction(func)
+    return memoized_function
 
 def rename_on_init(name: str) -> Callable:
     def decorator(func: Callable) -> Callable:
