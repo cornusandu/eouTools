0927d51b573adf29d6802c9bba053182db9e3c1d
cornusandu <cornusandu@hotmail.com>
2024-07-15 15:02:39 +0300

    Update v0.0.6.1

--- eouTools/arithmetic_equations/integers.py
+++ eouTools/arithmetic_equations/integers.py
@@ -1,4 +1,9 @@
-import numpy as np
+try:
+    import numpy as np
+except ImportError as _:
+    np = None
+
+import math
 
 def factorial(n: int) -> int:
     """
@@ -6,9 +11,13 @@ def factorial(n: int) -> int:
     :param n: int
     :returns: int
         """
-    n = np.abs(n)
+    if np:
+        n = np.abs(n)
 
-    return np.prod(np.arange(1, n + 1, dtype = int))
+        return np.prod(np.arange(1, n + 1, dtype=int))
+    else:
+        n = abs(n)
+        return math.prod(range(1, n + 1))
 
 def fib(n: int) -> int:
     """
@@ -16,7 +25,10 @@ def fib(n: int) -> int:
     :param n: int
     :returns: int
     """
-    n = np.abs(n)
+    if np:
+        n = np.abs(n)
+    else:
+        n = abs(n)
     if n < 2:
         return n
     return fib(n - 1) + fib(n - 2)
